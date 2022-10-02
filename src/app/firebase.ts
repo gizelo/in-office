deed9bb273be4cd60374812cc24a5740e4143b31
gizelo <robertgizelo25@gmail.com>
2022-10-02 21:35:01 +0200

    upd2

---
+++ src/app/firebase.ts
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBiLrUKOV9nqyHuk9AmtBXohXSICpeDQGY",
+    authDomain: "in-office-863ec.firebaseapp.com",
+    projectId: "in-office-863ec",
+    storageBucket: "in-office-863ec.appspot.com",
+    messagingSenderId: "711326671628",
+    appId: "1:711326671628:web:d8dd34824940f5c8665f29",
+    measurementId: "G-M3H4CMZMP7"
+};
+
+const app = initializeApp(firebaseConfig);
